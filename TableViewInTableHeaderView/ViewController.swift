03bbd739c7cca5a54b02c04668a87c0b8e6c4ebe
Alexey Ivanov <alexey.ivanov>
2016-08-30 21:07:05 +0300

    Initial Commit

---
+++ TableViewInTableHeaderView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableViewInTableHeaderView
+//
+//  Created by Alexey Ivanov on 30.08.16.
+//  Copyright © 2016 Alexey Ivanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
